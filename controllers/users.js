46883af8d97cc71441df6e7d71c118598eedcea1
Vitaly Tsyganov <format911@yandex.ru>
2023-07-20 22:24:54 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -73,6 +73,9 @@ module.exports.updateUser = (req, res, next) => {
       res.send(user);
     })
     .catch((err) => {
+      if (err.code === 11000) {
+        return next(new ConflictError('Пользователь с указанным e-mail уже существует'));
+      }
       if (err.name === 'CastError' || err.name === 'ValidationError') {
         return next(new BadRequestError('Переданы некорректные данные при обновлении пользователя'));
       }
